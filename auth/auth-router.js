9546fb5850e2990ac80ae25752e87434668e8772
Zealll <eriznis@gmail.com>
2019-04-10 17:42:26 -0400

    Finished the Guided demo

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,5 +1,8 @@
 const router = require('express').Router();
 const bcrypt = require('bcryptjs');
+const jsonWT = require('jsonwebtoken') 
+
+const secret = require('../config/secrets.js')
 
 const Users = require('../users/users-model.js');
 
@@ -25,8 +28,10 @@ router.post('/login', (req, res) => {
     .first()
     .then(user => {
       if (user && bcrypt.compareSync(password, user.password)) {
+        const token = generateToken(user)
+
         res.status(200).json({
-          message: `Welcome ${user.username}!`,
+          message: `Welcome ${user.username}!`, token
         });
       } else {
         res.status(401).json({ message: 'Invalid Credentials' });
@@ -36,5 +41,20 @@ router.post('/login', (req, res) => {
       res.status(500).json(error);
     });
 });
+
+
+
+function generateToken(user) {
+  const payload = {
+    subject: user.id, 
+    username: user.username
+  }
+
+  const options = {
+    expiresIn: '1d'
+  }
+
+  return jsonWT.sign(payload, secret.jwtSecret, options)
+}
 
 module.exports = router;
